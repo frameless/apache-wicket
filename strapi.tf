50d994bc08030dc652588552f887926430abe135
Robbert Broersma <git@robbertbroersma.nl>
2024-07-10 14:54:09 +0200

    ci: update branch protection ID, matrix was removed

--- strapi.tf
+++ strapi.tf
@@ -47,7 +47,7 @@ resource "github_branch_protection" "strapi-main" {
   required_status_checks {
     # Require branches to be up to date before merging
     strict   = true
-    contexts = ["continuous-integration (20.x)", "continuous-deployment"]
+    contexts = ["continuous-integration", "continuous-deployment"]
   }
 
   required_pull_request_reviews {
